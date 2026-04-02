990859938ed318c7870c70f0eea0af9a409afd9a
Kittipoom <kittipoomnak02@gmail.com>
2026-04-02 22:44:20 +0700

    feat: navigation, app, main

---
+++ lib/features/grade/grade_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class GradeScreen extends StatelessWidget {
+  const GradeScreen({super.key});
+  @override
+  Widget build(BuildContext context) =>
+      const Center(child: Text('Grade — Coming Soon'));
+}
---
+++ lib/features/note/note_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NoteScreen extends StatelessWidget {
+  const NoteScreen({super.key});
+  @override
+  Widget build(BuildContext context) =>
+      const Center(child: Text('Note — Coming Soon'));
+}
